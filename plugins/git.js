c0f36bf1cd549d2deb016272e787df48de93eb84
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-10-12 10:36:14 +0530

    ippo sheriyakkithera 🤠

--- plugins/git.js
+++ plugins/git.js
@@ -5,24 +5,19 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var r_text = new Array ();
-    
-    
-   
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
-    
-    
-    var i = Math.floor(1*Math.random())
+    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
 *Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+
 *Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+
 *Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+
 *Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
 `}) 
 
