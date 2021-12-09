5dd5646b4642c026311d1e7199552166bababa7c
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-09 16:54:49 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -80,3 +80,4 @@ else if (Config.WORKTYPE == 'public') {
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }
+ 
