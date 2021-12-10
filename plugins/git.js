3aab6c3616adc6a89d36f0ebdc6b79a823f81300
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-10 09:58:59 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,22 +7,11 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/YC766jA.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*OXYMORON bot created by AS*
+*Creator number : http://wa.me/919605101516?text=Hi%20im-coming%20your-bot-git-oxymoron%20*
 
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
-
-*For More Updates Subscribe The Channel ☝*
 `}) 
 
 }));
