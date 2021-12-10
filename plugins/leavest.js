14c845c4c89843ae59aa3bc1cb04fff5defc812a
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-10 09:34:34 +0530

    Update leavest.js

--- plugins/leavest.js
+++ plugins/leavest.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 ameer-kallumthodi.
+/* Copyright (C) 2021 kings as.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-PIKACHU*/
+OXYMORON*/
 
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
@@ -16,9 +16,9 @@ if (Config.WORKTYPE == 'private') {
 
     if (match[1] === '') return await message.sendMessage(need);
 
-    var ttinullimage = await axios.get(`https://api.zeks.xyz/api/leavest?apikey=Upe1Fp1lDAtX0ioPYLEPsSoX51i&text1=Pikachu&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://api.zeks.xyz/api/leavest?apikey=Upe1Fp1lDAtX0ioPYLEPsSoX51i&text1=OXYMORON&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Pikachu*' })
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by OXYMORON*' })
 
   }));
 }
@@ -29,9 +29,9 @@ else if (Config.WORKTYPE == 'public') {
 
     if (match[1] === '') return await message.sendMessage(need);
 
-    var ttinullimage = await axios.get(`https://api.zeks.xyz/api/leavest?apikey=Upe1Fp1lDAtX0ioPYLEPsSoX51i&text1=Pikachu&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://api.zeks.xyz/api/leavest?apikey=Upe1Fp1lDAtX0ioPYLEPsSoX51i&text1=OXYMORON&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Pikachu*' })
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by OXYMORON*' })
 
   }));
 
