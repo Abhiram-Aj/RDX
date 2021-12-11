2602d9fb85ade477e0e4146236b7b04309001657
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-11 08:41:20 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,8 +9,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://i.imgur.com/YC766jA.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*OXYMORON bot created by AS*
-*Creator number : http://wa.me/919605101516?text=Hi%20im-coming%20your-bot-git-oxymoron%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: 𝐎𝐗𝐘𝐌𝐎𝐑𝐎𝐍 𝐁𝐎𝐓 𝐂𝐑𝐄𝐀𝐓𝐄𝐃 𝐁𝐘 𝐊𝐈𝐍𝐆𝐒-𝐀𝐒
+
+𝐂𝐑𝐄𝐀𝐓𝐎𝐑 𝐍𝐔𝐌𝐁𝐄𝐑 http://wa.me/919605101516?text=Hi%20im-coming%20your-bot-git-oxymoron%20
+
+𝐆𝐈𝐓 𝐋𝐈𝐍𝐊 https://github.com/KINGS-AS/OXYMORON
+
+🍃എന്നെ കꪆത്ത᭄ര᭄ക്കꪆൻ രꪆജ്യമോ രꪆജകുമꪆര᭄യോ ഇല്ല🥀💫 പകരം എ൱᭄ക്ക് ൨േണ്ട᭄ ചങ്ക് പറച്ചു തെരുന്ന കുറച്ചു ചങ്കന്മꪆർ കു‌ടെ ഉണ്ട്🌝അതു മതി എ൱᭄ക്കྂ♥
 
 `}) 
 
