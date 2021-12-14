04e4ba977dfa05f8d6a4cd4cca29aeb9ba787331
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-14 15:39:45 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,10 +7,10 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/YC766jA.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://telegra.ph/file/c14a8dc543d2815bf558c.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*OXYMORON bot created by ABHIRAM SACHU*
-*Creator number : wa.me/919605101516?text=Hi%20im_coming_your%20oxymoron_git_link.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*OXYMORON Bot Created By ABHIRAM SACHU*
+*Creator number : wa.me/919605101516?text=Hi%20im_coming_your%20oxymoron_git_massage.%20*
 
 *Githublink (Setup)  :    https://github.com/KINGS-AS/OXYMORON*
 
