569bbec33070f5abdd85304b0e1c49239fbdeb6e
RAJU BHAI <96112015+DEVIL-SARATH@users.noreply.github.com>
2021-12-14 21:21:45 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,10 +7,13 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://telegra.ph/file/c14a8dc543d2815bf558c.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://telegra.ph/file/5c4082bd35a3866b47978.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*OXYMORON Bot Created By ABHIRAM SACHU*
-*Creator number : wa.me/919605101516?text=Hi%20im_coming_your%20oxymoron_git_massage.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*RAJU-BHAI Bot Created By ABHIRAM SACHU AND SARATH ACHU*
+
+*Creator number : wa.me/919496306657?text=Hi%20im_coming_your%20RAJU-BHAI_git_massage.%20*
+
+*Creator number 2 : wa.me/919605101516?text=Hi%20im_coming_your%20RAJU-BHAI_git_massage.%20*
 
 *Githublink (Setup)  :    https://github.com/KINGS-AS/OXYMORON*
 
