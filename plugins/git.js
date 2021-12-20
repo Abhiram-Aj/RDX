e06ea0118aca844de62b572a0ef1061594c39f6c
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-20 20:53:22 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,11 +9,14 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://telegra.ph/file/c14a8dc543d2815bf558c.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*OXYMORON Bot Created By ABHIRAM SACHU*
-*Creator number : wa.me/919605101516?text=Hi%20im_coming_your%20oxymoron_git_massage.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*RDX Bot Created By Abhiram Sachu*
+*Creator number : wa.me/919605101516?text=Hi%20im_coming_your%20RDX_git_massage.%20*
 
-*Githublink (Setup)  :    https://github.com/KINGS-AS/OXYMORON*
+*Githublink (Setup)  :    https://github.com/KINGS-AS/RDX*
 
+*QR
+
+*DEPLOYE
 
 `}) 
 
