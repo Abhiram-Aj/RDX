40a4c931a5bae3246763fddb03e013ea1d9ee03c
As abhiram sachu <95849340+KINGS-AS@users.noreply.github.com>
2021-12-22 10:05:12 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,7 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/6ZluhO1.mp4", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://telegra.ph/file/5b1a13030d82fce99f12f.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `RDX Bot Created By Abhiram Sachu
 
