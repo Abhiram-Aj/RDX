6408ccf961e950aa347c02e65cb89712555e1115
AS <95849340+KINGS-AS@users.noreply.github.com>
2022-01-02 13:25:17 +0530

    Nothing

--- plugins/git.js
+++ plugins/git.js
@@ -10,13 +10,9 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://telegra.ph/file/5c4082bd35a3866b47978.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*RAJU-BHAI Bot Created By ABHIRAM SACHU AND SARATH ACHU*
-
-*Creator number : wa.me/919496306657?text=Hi%20im_coming_your%20RAJU-BHAI_git_massage.%20*
-
-*Creator number 2 : wa.me/919605101516?text=Hi%20im_coming_your%20RAJU-BHAI_git_massage.%20*
-
-*Githublink (Setup)  :    https://github.com/DEVIL-SARATH/RAJU-BHAI*
-
+This a Private bot 
+*PUBLIC BOT (LINK)  :    https://github.com/DEVIL-SARATH/RajuBhai*
+ PUBLIC BOT 2 (LINK)   :  https://github.com/KINGS-AS/RDX
 
 `}) 
 
