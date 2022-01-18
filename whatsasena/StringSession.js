8ff43e362d85d1ab8285321fc35bb8e26592cfce
AS <95849340+KINGS-AS@users.noreply.github.com>
2022-01-18 19:20:24 +0530

    Noting

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'RDXQR;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
