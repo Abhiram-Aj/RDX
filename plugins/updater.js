fbd40455d574a86aacbb0c86b92673b4e04ba8f1
KING-AS <98018566+KING-AS@users.noreply.github.com>
2022-01-19 15:21:31 +0530

    Update

--- plugins/updater.js
+++ plugins/updater.js
@@ -32,7 +32,7 @@ Asena.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, de
         var degisiklikler = Lang.NEW_UPDATE;
         commits['all'].map(
             (commit) => {
-                degisiklikler += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
+                degisiklikler += '♦️ [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
             }
         );
         
